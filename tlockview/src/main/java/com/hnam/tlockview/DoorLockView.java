bb0b4d5a578da290559f94ceba2d9402affd8d67
Nam Pham <nam.phho@gmail.com>
2019-02-12 15:27:52 +0700

    [dev] radar animation

--- tlockview/src/main/java/com/hnam/tlockview/DoorLockView.java
+++ tlockview/src/main/java/com/hnam/tlockview/DoorLockView.java
@@ -1,11 +1,18 @@
 package com.hnam.tlockview;
 
 import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -14,10 +21,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static android.view.MotionEvent.ACTION_UP;
 import static com.hnam.tlockview.DoorLockState.STATE_OFFLINE;
@@ -28,18 +38,30 @@ import static com.hnam.tlockview.DoorLockState.STATE_OFFLINE;
 public class DoorLockView extends RelativeLayout {
     private static final String TAG = DoorLockView.class.getSimpleName();
 
-    public DoorLockView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-
     private View mBg;
     private View mIndicator;
     private DoorLockState mState = STATE_OFFLINE;
+    private int doorViewColor;
 
+    public DoorLockView(Context context) {
+        super(context);
+        init(context, null);
+    }
 
-    private int minDimension;
+    public DoorLockView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
 
+    public void init(Context context, AttributeSet attrs){
+        if (attrs != null){
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.DoorLockView);
+            doorViewColor = a.getColor(R.styleable.DoorLockView_dv_color, getResources().getColor(R.color.colorAccent));
+            a.recycle();
+        } else {
+            doorViewColor = getResources().getColor(R.color.colorAccent);
+        }
+    }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -77,10 +99,10 @@ public class DoorLockView extends RelativeLayout {
         mBg.layout(left, top, right, bottom);
         //place ripple
         if (rippleView != null) {
-//            left = (int) (getPivotX() - Math.round(rippleView.getMeasuredWidth() / 2f));
-//            top = (int) ((int) getPivotY() - Math.round(rippleView.getMeasuredHeight() / 2f));
-//            right = (int) (getPivotX() + Math.round(rippleView.getMeasuredWidth() / 2f));
-//            bottom = (int) ((int) getPivotY() + Math.round(rippleView.getMeasuredHeight() / 2f));
+            left = (int) (getPivotX() - Math.round(rippleView.getMeasuredWidth() / 2f));
+            top = (int) ((int) getPivotY() - Math.round(rippleView.getMeasuredHeight() / 2f));
+            right = (int) (getPivotX() + Math.round(rippleView.getMeasuredWidth() / 2f));
+            bottom = (int) ((int) getPivotY() + Math.round(rippleView.getMeasuredHeight() / 2f));
             rippleView.layout(left, top, right, bottom);
         }
 
@@ -105,24 +127,7 @@ public class DoorLockView extends RelativeLayout {
         Log.e(TAG, "onFinishInflate");
         mBg = (View) findViewById(R.id.iv_bg);
         mIndicator = (View) findViewById(R.id.view_indicator);
-        //rippleView = findViewById(R.id.ripple);
-//        LayoutParams bgParams = (LayoutParams) mBg.getLayoutParams();
-//        bgParams.width = LayoutParams.MATCH_PARENT;
-//        bgParams.height = LayoutParams.MATCH_PARENT;
-//        bgParams.addRule(CENTER_IN_PARENT);
-//        mBg.setLayoutParams(bgParams);
-//        mBg.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_gray_circle));
-//
-//        this.post(new Runnable() {
-//            @Override
-//            public void run() {
-//                LayoutParams tvParams = (LayoutParams) mIndicator.getLayoutParams();
-//                tvParams.height = (int) (minDimension * 0.8f);
-//                tvParams.addRule(CENTER_IN_PARENT);
-//                mIndicator.setLayoutParams(tvParams);
-//            }
-//        });
-
+        rippleView = (MyCircleView) findViewById(R.id.ripple);
 
         mBg.setOnTouchListener(new OnTouchListener() {
             @Override
@@ -170,7 +175,15 @@ public class DoorLockView extends RelativeLayout {
     public void control(DoorLockState state) {
         this.mState = state;
         rotate();
-        toggleRipple();
+        if (state == DoorLockState.STATE_UNLOCK){
+            rippleView.stopRipple();
+        } else if (state == DoorLockState.STATE_LOCK){
+            rippleView.startRipple();
+        }
+    }
+
+    private void runRipple(){
+
     }
 
     private void setDoorState(DoorLockState state) {
@@ -190,12 +203,22 @@ public class DoorLockView extends RelativeLayout {
     }
 
     private void setColor(DoorLockState state) {
+        Drawable bgDrawable = ContextCompat.getDrawable(getContext(), R.drawable.bg_red_circle);
+        Drawable idDrawable = ContextCompat.getDrawable(getContext(), R.drawable.bg_red_indicator_shadow);
+        PorterDuffColorFilter filter = new PorterDuffColorFilter(doorViewColor, PorterDuff.Mode.SRC_IN);
+        Objects.requireNonNull(bgDrawable).setColorFilter(filter);
+        Objects.requireNonNull(idDrawable).setColorFilter(filter);
+
         if (state == DoorLockState.STATE_LOCK) {
-            mBg.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_red_circle));
-            mIndicator.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_red_indicator_shadow));
+            //mBg.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_red_circle));
+            //mIndicator.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_red_indicator_shadow));
+            mBg.setBackground(bgDrawable);
+            mIndicator.setBackground(idDrawable);
         } else if (state == DoorLockState.STATE_UNLOCK) {
-            mBg.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_green_circle));
-            mIndicator.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_green_indicator_shadow));
+            //mBg.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_green_circle));
+            //mIndicator.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_green_indicator_shadow));
+            mBg.setBackground(bgDrawable);
+            mIndicator.setBackground(idDrawable);
         } else {
             mBg.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_gray_circle));
             mIndicator.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_gray_indicator_shadow));
@@ -255,27 +278,7 @@ public class DoorLockView extends RelativeLayout {
                     .rotation(0)
                     .setInterpolator(new AccelerateDecelerateInterpolator())
                     .setDuration(500)
-                    .setListener(new Animator.AnimatorListener() {
-                        @Override
-                        public void onAnimationStart(Animator animator) {
-
-                        }
-
-                        @Override
-                        public void onAnimationEnd(Animator animator) {
-                            setColor(mState);
-                        }
-
-                        @Override
-                        public void onAnimationCancel(Animator animator) {
-
-                        }
-
-                        @Override
-                        public void onAnimationRepeat(Animator animator) {
-
-                        }
-                    })
+                    .setListener(animatorListener)
                     .start();
             //green
         } else if ((mState == DoorLockState.STATE_LOCK || mState == DoorLockState.STATE_CONTROL_LOCK) && rotation != 90.f) {
@@ -283,108 +286,18 @@ public class DoorLockView extends RelativeLayout {
                     .rotation(90)
                     .setInterpolator(new AccelerateDecelerateInterpolator())
                     .setDuration(500)
-                    .setListener(new Animator.AnimatorListener() {
-                        @Override
-                        public void onAnimationStart(Animator animator) {
-
-                        }
-
-                        @Override
-                        public void onAnimationEnd(Animator animator) {
-                            setColor(mState);
-                        }
-
-                        @Override
-                        public void onAnimationCancel(Animator animator) {
-
-                        }
-
-                        @Override
-                        public void onAnimationRepeat(Animator animator) {
-
-                        }
-                    })
+                    .setListener(animatorListener)
                     .start();
             //red
         }
     }
 
-    private void toggleRipple(){
-        if (rippleView == null) {
-            rippleView = new MyCircleView(getContext());
-            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-            rippleView.setLayoutParams(params);
-            addView(rippleView);
-            rippleView.post(new Runnable() {
-                @Override
-                public void run() {
-                    rippleView.runRipple();
-                }
-            });
-        } else {
-            rippleView.stop();
-            removeViewAt(getChildCount() - 1);
-            rippleView = null;
+    private AnimatorListenerAdapter animatorListener = new AnimatorListenerAdapter() {
+        @Override
+        public void onAnimationEnd(Animator animation) {
+            super.onAnimationEnd(animation);
         }
-
-    }
-
-
-    private void runRipple(final View view){
-
-        ObjectAnimator objAnim = ObjectAnimator.ofFloat(view, View.SCALE_X, 30);
-        ObjectAnimator objAnim1 = ObjectAnimator.ofFloat(view, View.SCALE_Y, 30);
-        ObjectAnimator objAnim2 = ObjectAnimator.ofFloat(view, View.ALPHA, 0.3f);
-
-
-
-        List<Animator> list = new ArrayList<>();
-        list.add(objAnim);
-        list.add(objAnim1);
-        list.add(objAnim2);
-
-
-        AnimatorSet set = new AnimatorSet();
-        set.playTogether(list);
-        set.setInterpolator(new AccelerateInterpolator());
-        set.setDuration(1000);
-
-
-//        AnimatorSet setReturn = new AnimatorSet();
-//        setReturn.playTogether(list1);
-//        setReturn.setInterpolator(new AccelerateInterpolator());
-//        setReturn.setDuration(1000);
-//
-//        AnimatorSet result = new AnimatorSet();
-//        result.playSequentially(set, setReturn);
-
-        set.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-                Log.e(TAG,"onAnimationStart");
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                Log.e(TAG,"onAnimationEnd");
-                removeViewAt(getChildCount() - 1);
-
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-                Log.e(TAG,"onAnimationCancel");
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-                Log.e(TAG,"onAnimationRepeat");
-            }
-        });
-        set.start();
-    }
-
-
+    };
 
 
 }
--- tlockview/src/main/java/com/hnam/tlockview/MyCircleView.java
+++ tlockview/src/main/java/com/hnam/tlockview/MyCircleView.java
@@ -1,13 +1,17 @@
 package com.hnam.tlockview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+
+import java.util.Timer;
 
 /**
  * Created by nampham on 2/11/19.
@@ -18,25 +22,34 @@ public class MyCircleView extends View {
 
     public MyCircleView(Context context) {
         super(context);
-        init();
+        init(context, null);
     }
 
     public MyCircleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context, attrs);
     }
 
     public MyCircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(context, attrs);
     }
 
     private Paint paint;
-    public void init(){
+    private int color;
+    public void init(Context context, AttributeSet attrs){
+        if (attrs != null){
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.MyCircleView);
+            color = a.getColor(R.styleable.MyCircleView_cv_color, ContextCompat.getColor(context, R.color.colorAccent));
+            a.recycle();
+        } else {
+            color = ContextCompat.getColor(context, R.color.colorAccent);
+        }
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setAntiAlias(true);
         paint.setStyle(Paint.Style.FILL);
-        paint.setColor(ContextCompat.getColor(getContext(), R.color.colorAccent));
+        Log.e(TAG,"init paint="+color);
+        paint.setColor(color);
 
     }
 
@@ -47,41 +60,55 @@ public class MyCircleView extends View {
         super.onDraw(canvas);
         radius = Math.min(getWidth(), getHeight());
         if (index !=-1){
-            Log.e(TAG, "radius=%s" + radius + "index=" + index * radius/10);
-            //®Log.e(TAG, "alpha" + (float)(alpha));
             paint.setAlpha((int) (alpha * 255));
-            canvas.drawCircle(radius / 2f, radius / 2f, index * radius/10 * 0.5f, paint);
+            canvas.drawCircle(radius / 2f, radius / 2f, index * radius/FRAME_INDEX * 0.5f, paint);
         } else {
-            Log.e(TAG, "jump here");
-            //®Log.e(TAG, "alpha" + (float)(alpha));
             paint.setAlpha((int) (alpha * 255));
             canvas.drawCircle(radius / 2f, radius / 2f, radius*0.5f, paint);
         }
 
     }
-
+    private final static int FRAME_INDEX = 20;
     private int index = -1;
     private float alpha = 1;
-    public void runRipple(){
+    private boolean isRunning = false;
+    public void startRipple(){
+        if (!isRunning) {
+            setVisibility(View.VISIBLE);
+            isRunning = true;
+            runRipple();
+        }
+    }
+
+    private void runRipple(){
         index++;
-        alpha = (10-index)/10f;
-        if (index == 11){
+        alpha = (FRAME_INDEX-index)/(float)FRAME_INDEX;
+        if (index == FRAME_INDEX+1){
             index = 0;
             alpha = 1;
         }
 
         invalidate();
-        postDelayed(startRunnable,100);
+        postDelayed(startRunnable,50);//chạy 1 sẽ vẽ hết được vòng tròn full size
     }
 
-    public void stop(){
+    public void stopRipple(){
         removeCallbacks(startRunnable);
+        setVisibility(View.GONE);
+        index = -1;
+        alpha = 1;
+        isRunning = false;
+    }
+
+    public boolean isRunning(){
+        return isRunning;
     }
 
     private Runnable startRunnable = new Runnable() {
         @Override
         public void run() {
             runRipple();
+
         }
     };
 }
